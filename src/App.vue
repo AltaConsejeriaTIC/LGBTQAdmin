cac008fa6a70649b060a0658a8dc754f440e9d87
Camilo <capinzonr@unal.edu.co>
2018-09-21 16:04:33 -0500

    Create back-ground color and form component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  background-color: #409FDC;
+}
+#app {
+  margin-top: 60px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="" class="container">
+    <div>
+      <form >
+        <div class="form-group">
+          <label for="first_name">Nombre:</label>
+          <input v-model="first_name" type="text" id="first_name" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="last_name">Apellido</label>
+          <input v-model="last_name" type="text" id="last_name" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary" v-bind:disabled= "!isFomrValid()">Continuar</button> -->
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      first_name: '',
+      last_name: ''
+    }
+  },
+  methods: {
+    isFormValid: function () {
+      return this.first_name != '' && this.last_name != '';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
