7ae3f0e5271c78de7bab8efa94ff74df5a134b21
Camilo <capinzonr@unal.edu.co>
2018-11-15 15:59:00 -0500

    handling routes in dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,73 @@ import DetailEvent from '@/components/Event/DetailEvent';
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/event/:id',
-      name: 'DetailEvent',
-      component: DetailEvent
+// export default new Router({
+//     routes: [{
+//             path: '*',
+//             redirect: '/login'
+//         },
+//         {
+//             path: '/',
+//             redirect: '/login'
+//         },
+//         {
+//             path: '/login',
+//             name: 'Login',
+//             component: Login
+//         },
+//         {
+//             path: '/dashboard',
+//             name: 'Dashboard',
+//             component: Dashboard
+//         },
+//         {
+//             path: '/event/:id',
+//             name: 'DetailEvent',
+//             component: DetailEvent
+//         }
+//     ]
+// });
+
+const router = new Router({
+    routes: [{
+            path: '*',
+            redirect: '/login'
+        },
+        {
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/dashboard',
+            name: 'Dashboard',
+            component: Dashboard,
+            meta: {
+                autentificado: true
+            }
+
+        },
+        {
+            path: '/event/:id',
+            name: 'DetailEvent',
+            component: DetailEvent
+        }
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    let user = true;
+    let autorizacion = to.matched.some(record => record.meta.autentificado);
+
+    if (autorizacion && !user) {
+        next('login')
+    } else {
+        next()
     }
-  ]
-});
+})
+
+export default router;
