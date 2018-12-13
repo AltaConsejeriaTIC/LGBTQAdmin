15035ca81a40ed063f7cc1e0f87b566fc43994f3
Dago Alexander Fonseca Ramírez <daafonsecara@unal.edu.co>
2018-12-12 20:03:29 -0500

    logout works

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,80 +1,129 @@
 <template>
-  <div class="ui bottom attached segment pushable">
-    <div class="ui visible inverted left vertical sidebar menu">
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Event'}"> <i class="home icon"></i> Eventos
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'News'}">
-        <i class="home icon"></i> Noticias
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Alliance'}">
-        <i class="home icon"></i> Alianzas
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Organization'}">
-        <i class="home icon"></i> Organizaciones
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'PersonalData'}">
-        <i class="home icon"></i> Censo
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Complaint'}">
-        <i class="home icon"></i> Denuncia
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Home'}">
-        <i class="home icon"></i> Home-carousel
-      </router-link>
-    </div>
-    <div class="pusher">
-      <div class="ui basic segment">
-        <img src="../../assets/logo-alcaldia.png" width=30% height=30%>
-        <router-view></router-view>
+  <div>
+    <div class="ui secondary pointing menu">
+      <div class="headerCenter">
+        <div class="title">
+          <h3 class="mainTitle">EN BOGOTÁ SE PUEDE SER</h3>
+          <h5 class="subtitle">ADMINISTRADOR</h5>
+        </div>        
+        <div>
+          <button class="logout" @click="signout">
+            CERRAR SESIÓN
+          </button>
+        </div>
+      </div>
+    </div>      
+    <div class="ui bottom attached segment pushable">
+      <div class="ui visible inverted left vertical sidebar menu">
+        <router-link
+          tag="a"
+          class="item"
+          :to="{name: 'Event'}"> <i class="home icon"></i> Eventos
+        </router-link>
+        <router-link
+          tag="a"
+          class="item"
+          :to="{name: 'News'}">
+          <i class="home icon"></i> Noticias
+        </router-link>
+        <router-link
+          tag="a"
+          class="item"
+          :to="{name: 'Alliance'}">
+          <i class="home icon"></i> Alianzas
+        </router-link>
+        <router-link
+          tag="a"
+          class="item"
+          :to="{name: 'Organization'}">
+          <i class="home icon"></i> Organizaciones
+        </router-link>
+        <router-link
+          tag="a"
+          class="item"
+          :to="{name: 'PersonalData'}">
+          <i class="home icon"></i> Censo
+        </router-link>
+        <router-link
+          tag="a"
+          class="item"
+          :to="{name: 'Complaint'}">
+          <i class="home icon"></i> Denuncia
+        </router-link>
+        <router-link
+          tag="a"
+          class="item"
+          :to="{name: 'Home'}">
+          <i class="home icon"></i> Home-carousel
+        </router-link>
+      </div>
+      <div class="pusher">
+        <div class="ui basic segment">
+          <router-view></router-view>
+        </div>
       </div>
     </div>
-  </div>
+  </div>  
 </template>
 
 <script>
+  import { mapActions, mapGetters} from 'vuex';
+  import * as constants from '@/store/constants';
+
   export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    methods: {
+      ...mapActions({
+        logout: constants.SESSION_LOGOUT
+      }),
+      signout() {
+        this.logout();
+        this.$router.push('/login')
+      }
+    }
   }
+
 </script>
 
 <style scoped>
 .pushable {
-    height: 100vh;
+  height: 100vh;
 }
 body > .grid {
   height: 100%;
 }
-.image {
-  margin-top: 1100px;
-}
-.column {
-  max-width: 450px;
-}
 
-h1,
-h2 {
+.menu {
+  margin: 0px;
+  height: 8vh;  
+}
+.headerCenter { 
+  margin-left: 260px;
+  display: flex;
+  padding-right: 100px;
+  width: 100%;
+  align-items: center;
+  text-transform: uppercase;
+  color: white;
+}
+.title {  
+  flex: 1;
+  text-align: left;
+}
+.mainTitle{
+  font-weight: 800;
+}
+.subtitle{
+  font-weight: bold;
+  color: black;
+}
+.logout {
+  font-size: 14px;
+}
+h1, h3, h5 {
   font-weight: normal;
+  font-family: 'Nunito';
+  margin: 0px;
 }
 ul {
   list-style-type: none;
--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -62,7 +62,6 @@ export default {
   watch: {
     token(data) {
       if (this.isLogged) {
-        sessionStorage.setItem('token', this.token);
         this.$router.push({ name: 'Dashboard'  });
       }
     }
