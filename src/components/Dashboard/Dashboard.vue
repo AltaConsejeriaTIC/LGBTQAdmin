85d7330927218cfcab81f968b340c8587256c286
capinzor <capinzonr@unal.edu.co>
2018-12-13 07:35:54 -0500

    add styles to dashboard

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,90 +1,265 @@
 <template>
-  <div class="ui bottom attached segment pushable">
-    <div class="ui visible inverted left vertical sidebar menu">
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Event'}"> <i class="home icon"></i> Eventos
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'News'}">
-        <i class="home icon"></i> Noticias
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Alliance'}">
-        <i class="home icon"></i> Alianzas
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Organization'}">
-        <i class="home icon"></i> Organizaciones
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'PersonalData'}">
-        <i class="home icon"></i> Censo
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Complaint'}">
-        <i class="home icon"></i> Denuncia
-      </router-link>
-      <router-link
-        tag="a"
-        class="item"
-        :to="{name: 'Home'}">
-        <i class="home icon"></i> Home-carousel
-      </router-link>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav v-if = "active" id="sidebar">
+            <div class="sidebar-header">
+                <h3>DDS</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <!-- <p>Dummy Heading</p> -->
+                <li class="active">
+                    <a href="#">CARRUSEL</a>                    
+                </li>
+                <li>
+                    <a href="#">EVENTOS</a>
+                </li>
+                <li>
+                    <a href="#">NOTICIAS</a>                    
+                </li>
+                <li>
+                    <a href="#">ORGANIZACIONES</a>
+                </li>
+                <li>
+                    <a href="#">ALIANZAS</a>
+                </li>
+                <li>
+                    <a href="#">CENSO</a>
+                </li>
+                <li>
+                    <a href="#">DENUNCIAS</a>
+                </li>
+            </ul>            
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info" @click="toggle">
+                        <i class="fas fa-align-justify"></i>                        
+                    </button>
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">                                
+                                <a id="navlink" href="#">CERRAR SESIÓN</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <h2>DDS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Lorem ipeerrr olor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h3>Lorem Ipsum Dolor</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
     </div>
-    <div class="pusher">
-      <div class="ui basic segment">
-        <img src="../../assets/logo-alcaldia.png" width=30% height=30%>
-        <router-view></router-view>
-      </div>
-    </div>
-  </div>
+    
 </template>
 
 <script>
+  
   export default {
-    name: 'Dashboard'
+    name: 'Dashboard',   
+    data () {
+      return {
+        active: true
+      }
+    }, 
+    methods: {
+      toggle: function() {
+        this.active = !this.active
+        console.log('ACTIVE===',this.active)
+      }
+    },
   }
 </script>
 
 <style scoped>
-.pushable {
-    height: 100vh;
-}
-body > .grid {
-  height: 100%;
-}
-.image {
-  margin-top: 1100px;
-}
-.column {
-  max-width: 450px;
+@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"; */
+body {
+    font-family: 'Poppins', sans-serif;
+    background: #fafafa;
 }
 
-h1,
-h2 {
-  font-weight: normal;
+p {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.1em;
+    font-weight: 300;
+    line-height: 1.7em;
+    color: #999;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+a,
+a:hover,
+a:focus {
+    color: inherit;
+    text-decoration: none;
+    transition: all 0.3s;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.navbar {
+    padding: 15px 10px;
+    background: rgb(59, 79, 143);
+    border: none;
+    border-radius: 0;
+    margin: -20px;
+    margin-bottom: 40px;
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
 }
-a {
-  color: #42b983;
+
+.bg-light {
+    background-color: #009DE0 !important;
+}
+
+.navbar-btn {
+    box-shadow: none;
+    outline: none !important;
+    border: none;
+}
+#navlink {
+    color:#fafafa;
+}
+
+.line {
+    width: 100%;
+    height: 1px;
+    border-bottom: 1px dashed #ddd;
+    margin: 40px 0;
+}
+
+
+.wrapper {
+    display: flex;
+    width: 100%;
+    align-items: stretch;
+}
+
+#sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    background: #202122;
+    color: #fff;    
+}
+
+#sidebar.active {
+    margin-left: -250px;
+}
+
+#sidebar .sidebar-header {
+    padding: 20px;
+    background: #202122;
+}
+
+#sidebar ul.components {
+    padding: 20px 0;
+    border-bottom: 1px solid #141313;
+}
+
+#sidebar ul p {
+    color: #fff;
+    padding: 10px;
+}
+
+#sidebar ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+}
+
+#sidebar ul li a:hover {
+    color: #009DE0;
+    background: rgba(248, 251, 252, 0.1);
+}
+
+#sidebar ul li.active>a,
+a[aria-expanded="true"] {
+    color:  #009DE0;
+    background: rgba(248, 251, 252, 0.1);
+}
+
+a[data-toggle="collapse"] {
+    position: relative;
+}
+
+.dropdown-toggle::after {
+    display: block;
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    transform: translateY(-50%);
+}
+
+ul ul a {
+    font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #6d7fcc;
+}
+
+ul.CTAs {
+    padding: 20px;
+}
+
+ul.CTAs a {
+    text-align: center;
+    font-size: 0.9em !important;
+    display: block;
+    border-radius: 5px;
+    margin-bottom: 5px;
+}
+
+a.download {
+    background: #fff;
+    color: #7386D5;
+}
+
+a.article,
+a.article:hover {
+    background: #6d7fcc !important;
+    color: #fff !important;
+}
+
+
+#content {
+    width: 100%;
+    padding: 20px;
+    min-height: 100vh;
+    transition: all 0.3s;
+    background-color: #EAEAEC;
+}
+.btn-info{
+    background-color: #343a40;
+    border-color: rgb(100, 97, 97);
+}
+
+
+@media (max-width: 768px) {
+    #sidebar {
+        margin-left: -250px;
+    }
+    #sidebar.active {
+        margin-left: 0;
+    }
+    #sidebarCollapse span {
+        display: none;
+    }
 }
 </style>
