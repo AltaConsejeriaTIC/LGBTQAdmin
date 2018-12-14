339b9dac280933d22aa606d23215829af4d79339
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-12-13 21:49:09 -0500

    eventos

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <div class="ui secondary pointing menu">
-      <div class="headerCenter">
-        <div class="title">
-          <h3 class="mainTitle">EN BOGOTÁ SE PUEDE SER</h3>
-          <h5 class="subtitle">ADMINISTRADOR</h5>
+    <div >
+      <div >
+        <div>
+          <h3>EN BOGOTÁ SE PUEDE SER</h3>
+          <h5>ADMINISTRADOR</h5>
         </div>        
         <div>
-          <button class="logout" @click="signout">
+          <button @click="signout">
             CERRAR SESIÓN
           </button>
         </div>
       </div>
     </div>      
-    <div class="ui bottom attached segment pushable">
-      <div class="ui visible inverted left vertical sidebar menu">
+    <div>
+      <div class="menu">
         <router-link
           tag="a"
           class="item"
@@ -58,9 +58,7 @@
         </router-link>
       </div>
       <div class="pusher">
-        <div class="ui basic segment">
-          <router-view></router-view>
-        </div>
+          <router-view class="content"></router-view>
       </div>
     </div>
   </div>  
@@ -86,54 +84,13 @@
 </script>
 
 <style scoped>
-.pushable {
-  height: 100vh;
-}
-body > .grid {
-  height: 100%;
-}
+  .pusher{
+    background-color: #EAEAEC;
+  }
 
-.menu {
-  margin: 0px;
-  height: 8vh;  
-}
-.headerCenter { 
-  margin-left: 260px;
-  display: flex;
-  padding-right: 100px;
-  width: 100%;
-  align-items: center;
-  text-transform: uppercase;
-  color: white;
-}
-.title {  
-  flex: 1;
-  text-align: left;
-}
-.mainTitle{
-  font-weight: 800;
-}
-.subtitle{
-  font-weight: bold;
-  color: black;
-}
-.logout {
-  font-size: 14px;
-}
-h1, h3, h5 {
-  font-weight: normal;
-  font-family: 'Nunito';
-  margin: 0px;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .content{
+    width: 80%;
+    margin: 0 auto;
+    padding-top: 36px;
+  }
 </style>
